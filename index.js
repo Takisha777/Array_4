b1d6acb688170508d0ff4f176e6e4be17bd4cd4e
Takisha777 <alexey.kovtun.od@gmail.com>
2024-05-31 05:09:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const coffees = ['Latte', 'Cappuccino', 'Americano'];
+const prices = [1.5, 1, 2];
+const updatedPrices = prices.map((price)=>{return price + 0.5});
+coffees.forEach((name,index) => {alert(`Кофе ${name} сейчас стоит ${updatedPrices[index]} евро`)});
